a77b45134671f4ce0265ec8d1d34933723d50aca
Lesly Becerra <lbecerracampana@gmail.com>
2024-10-20 14:03:10 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+document.write("Hola Mundo" + "<br>");
+/*For es un ciclo el cual tiene 2 parámetros la funcion es repetir un bloque de código las veces necesarias 
+for(condición){
+ bloque de código
+
+}*/
+for(i=3; i<=15; i++){
+    if (i%2==0){
+        document.write("números par : " +i +"<br>")
+    }
+    //document.write("Hola desde jS"+"<br>");
+    //document.write(i + "<br>")
+}
